a3fea682d39dc1a551c4fbd6d8e4de22b433da29
Twilight0908 <nguyenquang00.it@gmail.com>
2023-09-07 15:25:13 +0700

    sign up - quang - 15:25 07/09

--- WebThueNha/login/login.js
+++ WebThueNha/login/login.js
@@ -27,3 +27,52 @@ let login = function () {
         window.location.href = "login-sign-up.html";
     });
 }
+
+let checkLogin = function () {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username !== "" && password !== "") {
+        login();
+    } else {
+        alert("ko");
+    }
+}
+
+let signUp = function () {
+    let usernameC = document.getElementById("usernameC").value;
+    let passwordC = document.getElementById("passwordC").value;
+    let roleId = 2;
+    let accountC = {username: usernameC, password: passwordC, role: {id: roleId}};
+
+    let settings = {
+        "url": "http://localhost:8080/account/add",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "data": JSON.stringify(accountC),
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        alert("tao thanh cong");
+        window.location.href = "login-sign-up.html";
+    }).fail(function (xhr, status, error) {
+        console.log("Error:", error);
+        alert("tao khong thanh cong");
+        window.location.href = "login-sign-up.html";
+    });
+}
+
+let checkSignUp = function () {
+    let usernameT = document.getElementById("usernameC").value;
+    let passwordT = document.getElementById("passwordC").value;
+
+    if (usernameT !== "" && passwordT !== "") {
+        signUp();
+    } else {
+        alert("ko");
+    }
+}
